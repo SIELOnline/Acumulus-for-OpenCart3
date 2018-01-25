507ef11e495e72747d9f9255794d8e87db0792df
Erwin Derksen <fietserwin@750928.no-reply.drupal.org>
2018-01-25 22:08:15 +0100

    5.0.5: Restructure to publish on OpenCart Marketplace; only module itself remains in this repository.

--- acumulus_customise_invoice.ocmod/upload/admin/language/en-gb/extension/module/acumulus_customise_invoice.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @file This file is used in the extension list
- * (admin/index.php?route=extension/module)
- */
-$_['heading_title'] = 'Customise Acumulus Invoice';
-$_['text_extension'] = 'Extensions';
-$_['text_edit'] = 'This module does not have any settings';
--- copy-oc3-example-to-dev.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Copy files in our folder structure to development installation.
-del D:\Projecten\Acumulus\OpenCart\www30\www\admin\controller\extension\module\acumulus_customise_invoice.php 2> nul
-mklink /H D:\Projecten\Acumulus\OpenCart\www30\www\admin\controller\extension\module\acumulus_customise_invoice.php D:\Projecten\Acumulus\Webkoppelingen\OpenCart3\acumulus_customise_invoice.ocmod\upload\admin\controller\extension\module\acumulus_customise_invoice.php
-del D:\Projecten\Acumulus\OpenCart\www30\www\admin\language\en-gb\extension\module\acumulus_customise_invoice.php 2> nul
-mklink /H D:\Projecten\Acumulus\OpenCart\www30\www\admin\language\en-gb\extension\module\acumulus_customise_invoice.php D:\Projecten\Acumulus\Webkoppelingen\OpenCart3\acumulus_customise_invoice.ocmod\upload\admin\language\en-gb\extension\module\acumulus_customise_invoice.php
-del D:\Projecten\Acumulus\OpenCart\www30\www\admin\view\template\extension\module\acumulus_customise_invoice.twig 2> nul
-mklink /H D:\Projecten\Acumulus\OpenCart\www30\www\admin\view\template\extension\module\acumulus_customise_invoice.twig D:\Projecten\Acumulus\Webkoppelingen\OpenCart3\acumulus_customise_invoice.ocmod\upload\admin\view\template\extension\module\acumulus_customise_invoice.twig
